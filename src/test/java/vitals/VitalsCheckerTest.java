09ce57701ab192bf29f706fd1d3e9d27a4632d9d
ArpithaPalavalli <ArpithaPalavalli.Venkatesulu@in.bosch.com>
2025-08-13 11:36:51 +0530

    Update VitalsCheckerTest.java

--- src/test/java/vitals/VitalsCheckerTest.java
+++ src/test/java/vitals/VitalsCheckerTest.java
@@ -1,7 +1,8 @@
 package vitals;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+ import static org.junit.Assert.assertFalse;
+ import static org.junit.Assert.assertTrue;
 
 class VitalsCheckerTest {
 
@@ -82,6 +83,7 @@ class VitalsCheckerTest {
 //     // assertFalse(VitalsChecker.vitalsOk(102f, 100, 90)); // Temperature and Pulse rate at maximum limits
 //    }
 // }
+
 
 
 
