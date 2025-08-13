d00650aa3a2c728316f36b1c677b88e5cf22d4ac
ArpithaPalavalli <ArpithaPalavalli.Venkatesulu@in.bosch.com>
2025-08-13 12:07:03 +0530

    Update VitalsCheckerTest.java

--- src/test/java/vitals/VitalsCheckerTest.java
+++ src/test/java/vitals/VitalsCheckerTest.java
@@ -9,49 +9,49 @@ public class VitalsCheckerTest {
     VitalsChecker checker = new VitalsChecker(message -> {});
 
     @Test
-    void temperatureWithinRangeIsNormal() {
+    public void temperatureWithinRangeIsNormal() {
         Vital temp = new TemperatureVital(95, 102);
         assertTrue(temp.isNormal(new VitalReading(98.6f, 70, 95)));
     }
 
     @Test
-    void temperatureBelowMinIsCritical() {
+    public void temperatureBelowMinIsCritical() {
         Vital temp = new TemperatureVital(95, 102);
         assertFalse(temp.isNormal(new VitalReading(94.9f, 70, 95)));
     }
 
     @Test
-    void temperatureAboveMaxIsCritical() {
+    public void temperatureAboveMaxIsCritical() {
         Vital temp = new TemperatureVital(95, 102);
         assertFalse(temp.isNormal(new VitalReading(102.1f, 70, 95)));
     }
 
     @Test
-    void pulseRateWithinRangeIsNormal() {
+    public void pulseRateWithinRangeIsNormal() {
         Vital pulse = new PulseRateVital(60, 100);
         assertTrue(pulse.isNormal(new VitalReading(98.6f, 80, 95)));
     }
 
     @Test
-    void pulseRateBelowMinIsCritical() {
+    public void pulseRateBelowMinIsCritical() {
         Vital pulse = new PulseRateVital(60, 100);
         assertFalse(pulse.isNormal(new VitalReading(98.6f, 59.9f, 95)));
     }
 
     @Test
-    void pulseRateAboveMaxIsCritical() {
+    public void pulseRateAboveMaxIsCritical() {
         Vital pulse = new PulseRateVital(60, 100);
         assertFalse(pulse.isNormal(new VitalReading(98.6f, 100.1f, 95)));
     }
 
     @Test
-    void spo2WithinRangeIsNormal() {
+    public void spo2WithinRangeIsNormal() {
         Vital spo2 = new SpO2Vital(90);
         assertTrue(spo2.isNormal(new VitalReading(98.6f, 80, 95)));
     }
 
     @Test
-    void spo2BelowMinIsCritical() {
+    public void spo2BelowMinIsCritical() {
         Vital spo2 = new SpO2Vital(90);
         assertFalse(spo2.isNormal(new VitalReading(98.6f, 80, 89.9f)));
     }
@@ -85,6 +85,7 @@ public class VitalsCheckerTest {
 //     // assertFalse(VitalsChecker.vitalsOk(102f, 100, 90)); // Temperature and Pulse rate at maximum limits
 //    }
 // }
+
 
 
 
