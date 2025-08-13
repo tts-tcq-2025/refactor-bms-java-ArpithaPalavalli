0b2eb52d3affe33bd8fae589eb41306b6f539dce
ArpithaPalavalli <ArpithaPalavalli.Venkatesulu@in.bosch.com>
2025-08-13 11:17:57 +0530

    Update VitalsChecker.java

--- src/main/java/vitals/VitalsChecker.java
+++ src/main/java/vitals/VitalsChecker.java
@@ -1,49 +1,33 @@
 package vitals;
 
-public abstract class VitalsChecker {
-    static final float MAX_TEMPERATURE = 102;
-    static final float MIN_TEMPERATURE = 95;
-    static final float MAX_PULSE_RATE = 100;
-    static final float MIN_PULSE_RATE = 60;
-    static final float MIN_SPO2 = 90;
+import java.util.ArrayList;
+import java.util.List;
 
-    static boolean vitalsOk(float temperature, float pulseRate, float spo2) throws InterruptedException {
-        boolean isOk = true;
+public class VitalsChecker {
 
-        isOk &= checkAndDisplayMessage(temperature, MIN_TEMPERATURE, MAX_TEMPERATURE, "Temperature is critical!");
-        isOk &= checkAndDisplayMessage(pulseRate, MIN_PULSE_RATE, MAX_PULSE_RATE, "Pulse Rate is out of range!");
-        isOk &= checkAndDisplayMessage(spo2, MIN_SPO2, Float.MAX_VALUE, "Oxygen Saturation out of range!");
+    private final List<Vital> vitals = new ArrayList<>();
+    private final CriticalMessageHandler messageHandler;
 
-        return isOk;
+    public VitalsChecker(CriticalMessageHandler messageHandler) {
+        this.messageHandler = messageHandler;
+        vitals.add(new TemperatureVital(95, 102));
+        vitals.add(new PulseRateVital(60, 100));
+        vitals.add(new SpO2Vital(90));
     }
 
-    private static boolean checkAndDisplayMessage(float value, float min, float max, String message) throws InterruptedException {
-        if (!checkValue(value, min, max)) {
-            displayCriticalMessage(message);
-            return false;
+    public boolean vitalsOk(float temperature, float pulseRate, float spo2) {
+        VitalReading reading = new VitalReading(temperature, pulseRate, spo2);
+        boolean allOk = true;
+
+        for (Vital vital : vitals) {
+            if (!vital.isNormal(reading)) {
+                messageHandler.handle(vital.getCriticalMessage());
+                allOk = false;
+            }
         }
-        return true;
-    }
-
-    private static boolean checkValue(float value, float min, float max) {
-        return value >= min && value <= max;
-    }
-
-    private static void displayCriticalMessage(String message) throws InterruptedException {
-        System.out.println(message);
-        animateWarning();
-    }
-
-    private static void animateWarning() throws InterruptedException {
-        for (int i = 0; i < 6; i++) {
-            System.out.print("\r* ");
-            Thread.sleep(1000);
-            System.out.print("\r *");
-            Thread.sleep(1000);
-        }
+        return allOk;
     }
 }
-
 
 
 
