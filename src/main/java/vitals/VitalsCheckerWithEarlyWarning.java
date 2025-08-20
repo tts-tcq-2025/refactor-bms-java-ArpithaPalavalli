727536825149b21aa132c182ff21ac7d8085de59
ArpithaPalavalli <ArpithaPalavalli.Venkatesulu@in.bosch.com>
2025-08-20 16:41:24 +0530

    Update VitalsCheckerWithEarlyWarning.java

--- src/main/java/vitals/VitalsCheckerWithEarlyWarning.java
+++ src/main/java/vitals/VitalsCheckerWithEarlyWarning.java
@@ -1,35 +1,19 @@
 package vitals;
 
-import java.util.List;
 import java.util.Arrays;
 
-public class VitalsCheckerWithEarlyWarning {
-    private final List<Vital> vitals;
-    private final CriticalMessageHandler messageHandler;
+public class VitalsCheckerWithEarlyWarning extends VitalsCheckerBase {
 
     public VitalsCheckerWithEarlyWarning(CriticalMessageHandler messageHandler) {
-        this.messageHandler = messageHandler;
-        this.vitals = Arrays.asList(
+        super(messageHandler, Arrays.asList(
             new TemperatureVital(95, 102),
             new PulseRateVital(60, 100),
             new SpO2Vital(90)
-        );
+        ));
     }
 
     public boolean vitalsOk(float temperature, float pulseRate, float spo2) {
-    VitalReading reading = new VitalReading(temperature, pulseRate, spo2);
-    return vitals.stream().allMatch(v -> checkVital(v, reading));
-}
-
-    private boolean checkVital(Vital vital, VitalReading reading) {
-        if (!vital.isNormal(reading)) {
-            messageHandler.handle(vital.getCriticalMessage());
-            return false;
-        }
-        String warning = vital.getWarningMessage(reading);
-         if (warning != null) {
-        messageHandler.handle(warning);
-    }
-        return true;
+        VitalReading reading = new VitalReading(temperature, pulseRate, spo2);
+        return vitals.stream().allMatch(v -> checkVital(v, reading));
     }
 }
