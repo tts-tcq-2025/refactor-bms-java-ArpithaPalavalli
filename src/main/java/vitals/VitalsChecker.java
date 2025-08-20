e2ec7bdbd617a37c2aa2c6e634c658eacbb1a417
ArpithaPalavalli <ArpithaPalavalli.Venkatesulu@in.bosch.com>
2025-08-20 16:41:11 +0530

    Update VitalsChecker.java

--- src/main/java/vitals/VitalsChecker.java
+++ src/main/java/vitals/VitalsChecker.java
@@ -3,37 +3,26 @@ package vitals;
 import java.util.ArrayList;
 import java.util.List;
 
-public class VitalsChecker {
-
-    private final List<Vital> vitals = new ArrayList<>();
-    private final CriticalMessageHandler messageHandler;
+public class VitalsChecker extends VitalsCheckerBase {
 
     public VitalsChecker(CriticalMessageHandler messageHandler) {
-        this.messageHandler = messageHandler;
+        super(messageHandler, createVitals());
+    }
+
+    private static List<Vital> createVitals() {
+        List<Vital> vitals = new ArrayList<>();
         vitals.add(new TemperatureVital(95, 102));
         vitals.add(new PulseRateVital(60, 100));
         vitals.add(new SpO2Vital(90));
+        return vitals;
     }
 
     public boolean vitalsOk(float temperature, float pulseRate, float spo2) {
         VitalReading reading = new VitalReading(temperature, pulseRate, spo2);
         boolean allOk = true;
-
         for (Vital vital : vitals) {
             allOk &= checkVital(vital, reading);
         }
         return allOk;
-    }
-
-    private boolean checkVital(Vital vital, VitalReading reading) {
-        if (!vital.isNormal(reading)) {
-            messageHandler.handle(vital.getCriticalMessage());
-            return false;
-        }
-        String warning = vital.getWarningMessage(reading);
-        if (warning != null) {
-            messageHandler.handle(warning);
-        }
-        return true;
     }
 }
