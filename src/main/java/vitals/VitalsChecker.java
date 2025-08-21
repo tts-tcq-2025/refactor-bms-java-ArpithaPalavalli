90fe530426821f7be3731fdbdd17d91a25de6fbf
ArpithaPalavalli <ArpithaPalavalli.Venkatesulu@in.bosch.com>
2025-08-21 08:56:29 +0530

    Update VitalsChecker.java

--- src/main/java/vitals/VitalsChecker.java
+++ src/main/java/vitals/VitalsChecker.java
@@ -10,17 +10,17 @@ public class VitalsChecker extends VitalsCheckerBase {
     }
 
     private static List<Vital> createVitals() {
-        List<Vital> vitals = new ArrayList<>();
-        vitals.add(new TemperatureVital(95, 102));
-        vitals.add(new PulseRateVital(60, 100));
-        vitals.add(new SpO2Vital(90));
-        return vitals;
+        List<Vital> vitalsList = new ArrayList<>();
+        vitalsList.add(new TemperatureVital(95, 102));
+        vitalsList.add(new PulseRateVital(60, 100));
+        vitalsList.add(new SpO2Vital(90));
+        return vitalsList;
     }
 
     public boolean vitalsOk(float temperature, float pulseRate, float spo2) {
         VitalReading reading = new VitalReading(temperature, pulseRate, spo2);
         boolean allOk = true;
-        for (Vital vital : vitals) {
+        for (Vital vital : monitoredVitals) {
             allOk &= checkVital(vital, reading);
         }
         return allOk;
