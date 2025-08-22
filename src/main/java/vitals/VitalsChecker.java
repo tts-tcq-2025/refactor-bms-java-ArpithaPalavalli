55f48f0cbb84d8c0dfb5159309151c8c935915b8
ArpithaPalavalli <ArpithaPalavalli.Venkatesulu@in.bosch.com>
2025-08-22 15:28:39 +0530

    Update VitalsChecker.java

--- src/main/java/vitals/VitalsChecker.java
+++ src/main/java/vitals/VitalsChecker.java
@@ -3,25 +3,27 @@ package vitals;
 import java.util.ArrayList;
 import java.util.List;
 
-public class VitalsChecker extends VitalsCheckerBase {
+public class VitalsChecker {
+    private final List<WarnableVital> vitals = new ArrayList<>();
+    private final CriticalMessageHandler messageHandler;
 
     public VitalsChecker(CriticalMessageHandler messageHandler) {
-        super(messageHandler, createVitals());
-    }
-
-    private static List<Vital> createVitals() {
-        List<Vital> vitalsList = new ArrayList<>();
-        vitalsList.add(new TemperatureVital(95, 102));
-        vitalsList.add(new PulseRateVital(60, 100));
-        vitalsList.add(new SpO2Vital(90));
-        return vitalsList;
+        this.messageHandler = messageHandler;
+        vitals.add(new TemperatureVital(95, 102));
+        vitals.add(new PulseRateVital(60, 100));
+        vitals.add(new SpO2Vital(90));
     }
 
     public boolean vitalsOk(float temperature, float pulseRate, float spo2) {
         VitalReading reading = new VitalReading(temperature, pulseRate, spo2);
         boolean allOk = true;
-        for (Vital vital : monitoredVitals) {
-            allOk &= checkVital(vital, reading);
+        for (WarnableVital vital : vitals) {
+            if (!vital.isNormal(reading)) {
+                messageHandler.handle(vital.getCriticalMessage());
+                allOk = false;
+            } else if (vital.isWarning(reading)) {
+                messageHandler.handle(vital.getWarningMessage(reading));
+            }
         }
         return allOk;
     }
